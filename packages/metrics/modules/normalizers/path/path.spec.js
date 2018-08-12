2e52e764aab3c4920888713b5ca32d69416ac513
tdeekens <tobias.deekens@commercetools.de>
2018-08-12 17:55:01 +0200

    refactor: add more specs to path normalization

--- packages/metrics/modules/normalizers/path/path.spec.js
+++ packages/metrics/modules/normalizers/path/path.spec.js
@@ -5,19 +5,21 @@ describe('normalizePath', () => {
 
   describe('with resources in path', () => {
     beforeEach(() => {
-      normalizedPath = normalizePath('/userId/123');
+      normalizedPath = normalizePath('/some/path/154/userId/ABC363AFE2');
     });
 
-    it('should not remove the first resource', () => {
+    it('should not remove the path resource', () => {
+      expect(normalizedPath).toEqual(expect.stringContaining('path'));
       expect(normalizedPath).toEqual(expect.stringContaining('userId'));
     });
 
-    it('should remove the subsequent resource', () => {
-      expect(normalizedPath).not.toEqual(expect.stringContaining('123'));
+    it('should the path value to the resource', () => {
+      expect(normalizedPath).not.toEqual(expect.stringContaining('154'));
+      expect(normalizedPath).not.toEqual(expect.stringContaining('ABC363AFE2'));
     });
 
     it('should insert placeholders for removed path fragments', () => {
-      expect(normalizedPath).toEqual('/userId/#val');
+      expect(normalizedPath).toEqual('/some/path/#val/userId/#val');
     });
   });
 
