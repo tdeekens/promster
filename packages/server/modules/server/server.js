050c35de0f0b4ce5f40e395395782ec14be3daaa
suǝʞǝǝpʇ <nerd@tdeekens.name>
2019-02-05 10:14:13 +0100

    fix(server): add rejection when server creation fails (#77)

--- packages/server/modules/server/server.js
+++ packages/server/modules/server/server.js
@@ -2,13 +2,16 @@ const http = require('http');
 const { getSummary, getContentType } = require('@promster/metrics');
 
 const createServer = (options = { port: 7788, hostname: '0.0.0.0' }) => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const server = http.createServer((req, res) => {
       res.writeHead(200, 'OK', { 'content-type': getContentType() });
       res.end(getSummary());
     });
 
-    server.listen(options.port, options.host, () => resolve(server));
+    server.listen(options.port, options.host, error => {
+      if (error) reject(error);
+      else resolve(server);
+    });
   });
 };
 
