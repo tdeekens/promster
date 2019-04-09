95cf88676a96f5c5a47c5a438764360ba6ef34d8
tdeekens <tobias.deekens@commercetools.de>
2019-04-09 13:12:11 +0200

    fix(express): to re-export signal setters

--- packages/express/modules/middleware/index.js
+++ packages/express/modules/middleware/index.js
@@ -1,7 +1,11 @@
 const {
   default: createMiddleware,
   getRequestRecorder,
+  signalIsUp,
+  signalIsNotUp,
 } = require('./middleware');
 
 exports.createMiddleware = createMiddleware;
+exports.signalIsUp = signalIsUp;
+exports.signalIsNotUp = signalIsNotUp;
 exports.getRequestRecorder = getRequestRecorder;
