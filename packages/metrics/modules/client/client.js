84fb5d2d1f95a315feb86658bec6d9ee63ac1ee0
tdeekens <tobias.deekens@commercetools.de>
2019-06-30 14:31:33 +0200

    refactor(metrics): to not assign default metrics collection

--- packages/metrics/modules/client/client.js
+++ packages/metrics/modules/client/client.js
@@ -1,11 +1,11 @@
 const Prometheus = require('prom-client');
 
-const collectDefaultMetrics = Prometheus.collectDefaultMetrics;
-// NOTE: This is the `globalRegistry` provided by the `prom-client`
-//       We could create multiple registries with `new Prometheus.registry()`.
+// NOTE:
+//   This is the `globalRegistry` provided by the `prom-client`
+//   We could create multiple registries with `new Prometheus.registry()`.
 const defaultRegister = Prometheus.register;
 
-collectDefaultMetrics();
+Prometheus.collectDefaultMetrics();
 
 exports.default = Prometheus;
 exports.defaultRegister = defaultRegister;
