a0a3b764f546ec70ed19d2d747500691b4b4b97b
tdeekens <tobias.deekens@commercetools.com>
2021-09-05 22:46:53 +0200

    chore: add demo server for testing

---
+++ demo/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { createMiddleware } = require('@promster/express');
+const {
+  createServer: createPrometheusMetricsServer,
+} = require('@promster/server');
+
+async function launchServer() {
+  const app = express();
+
+  app.use(createMiddleware());
+
+  const prometheusMetricsServer = await createPrometheusMetricsServer({
+    port: 8080,
+    detectKubernetes: false,
+  });
+
+  app.get('/', (req, res) => {
+    res.send('I am the server!')
+  })
+
+  const port = 80;
+  app.listen(port, () => {
+    console.log(`Express app listening at http://localhost:${port}`)
+  })
+
+  console.log(`Prometheus metrics available on http://localhost:8080`)
+}
+
+launchServer().catch(console.log)
