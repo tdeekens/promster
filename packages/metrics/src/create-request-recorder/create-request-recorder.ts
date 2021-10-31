a9f2a6723eb2489d0c07c18f7018804142d5a93d
tdeekens <tobias.deekens@commercetools.com>
2021-10-31 19:48:58 +0100

    fix: type

--- packages/metrics/src/create-request-recorder/create-request-recorder.ts
+++ packages/metrics/src/create-request-recorder/create-request-recorder.ts
@@ -12,7 +12,7 @@ type TRecorderAccuracy = 'ms' | 's';
 type TRecorderMetricType =
   | 'httpRequestsTotal'
   | 'httpRequestsHistogram'
-  | 'httpRequestsSummary';
+  | 'httpRequestsSummary'
   | 'httpContentLengthHistogram';
 type TRecordingOptions = {
   labels: TLabelValues;
